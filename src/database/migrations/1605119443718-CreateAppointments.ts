93532fcfb89b01c37578dc665ab32dd9bbc34490
Matheus Gonçalves <matheusglmonteiro@gmail.com>
2020-11-12 15:11:58 -0300

    finish database conexion

--- src/database/migrations/1605119443718-CreateAppointments.ts
+++ src/database/migrations/1605119443718-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1605119443718
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
