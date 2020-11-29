8c3fcc5780cd4214d72dadb17ed68c9254a4ed89
Matheus Gonçalves <matheusglmonteiro@gmail.com>
2020-11-29 20:37:30 -0300

    separating bussiness rules from infraestructure

--- src/modules/users/AuthenticateUserService.ts
+++ src/modules/users/AuthenticateUserService.ts
@@ -3,7 +3,7 @@ import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
 import authConfig from '../../config/auth';
-import User from '../../models/User';
+import User from './entities/User';
 
 import AppError from '../../errors/AppError';
 
