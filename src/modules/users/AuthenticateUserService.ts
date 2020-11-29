32255b237697ce565a3aa80db060a40209c41778
Matheus Gonçalves <matheusglmonteiro@gmail.com>
2020-11-29 15:48:22 -0300

    initiating a DDD metodology

--- src/modules/users/AuthenticateUserService.ts
+++ src/modules/users/AuthenticateUserService.ts
@@ -2,10 +2,10 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
-import authConfig from '../config/auth';
-import User from '../models/User';
+import authConfig from '../../config/auth';
+import User from '../../models/User';
 
-import AppError from '../errors/AppError';
+import AppError from '../../errors/AppError';
 
 interface Request {
   email: string;
--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import AuthenticateUserService from '../services/AuthenticateUserService';
+import AuthenticateUserService from '../modules/users/AuthenticateUserService';
 
 const sessionsRouter = Router();
 
